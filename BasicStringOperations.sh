4e2189441e7ab198868df3390762fb80fe26fab6
erika mayoute <e.mayoute44@gmail.com>
2022-02-14 11:10:59 +0100

    Substring

--- BasicStringOperations.sh
+++ BasicStringOperations.sh
@@ -1,3 +1,4 @@
+# !/bin/zsh
 # Dans cet exercice, vous devrez changer le dicton connu de Warren Buffett. Créez d'abord une variable ISAY et affectez-lui la valeur de dicton d'origine. Ensuite, réaffectez-la avec une nouvelle valeur modifiée en utilisant les opérations de chaîne et en suivant les 4 modifications définies : Change1 : remplacez la première occurrence de 'snow' par 'foot'. Change2 : supprimer la deuxième occurrence de 'neige'. Changement3 : remplacez « trouver » par « obtenir ». Change4 : supprimez tous les caractères après 'wet'. Astuce : Une façon d'implémenter Change4, si pour trouver l'index de 'w' dans le mot 'wet' et ensuite utiliser l'extraction de sous-chaîne.
 
 BUFFETT="Life is like a snowball. The important thing is finding wet snow and a really long hill."
