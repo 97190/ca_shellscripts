6dd994cacb7c40e540ec5a7ec1270ac1dcacbe3c
erika mayoute <e.mayoute44@gmail.com>
2022-02-14 10:45:24 +0100

    Array

--- Array.sh
+++ Array.sh
@@ -7,4 +7,16 @@
 # Afficher les nombres de la variable number
 numbers=123
 echo ${numbers}
+NAMES=( John Eric Jessica ) # c'est un tableau de type entier
+
+# write your code here
+NUMBERS=( 1 2 3 ) # le tableau NUMBERS contient les chiffres ( 1 2 3)
+
+STRINGS=( "hello" "world" ) # le tableau STRINGS contient les chaîne de caractères ("hello" "world")
+NumberOfNames=${#NAMES[@]} # la variable NumberOfName contient une méthode qui affiche le nombre de caractère dans la variable
+second_name=${NAMES[1]} # la variable second_name contient un index du tableau NAMES
+echo ${NUMBERS[@]} # imprime le contenu du tableau NUMBERS 1 2 3
+echo ${STRINGS[@]} # imprime le contenu du tableau STRINGS hello world
+echo "The number of names listed in the NAMES array: $NumberOfNames" #la variable affiche le résultat de cette longueur
+echo "The second name on the NAMES list is:" ${second_name} #la variable affiche le résultat récupérer par l'index
 
