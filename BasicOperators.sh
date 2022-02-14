769de7d7d2274ca7cbff376d422ed3b5b806e1a0
erika mayoute <e.mayoute44@gmail.com>
2022-02-14 10:55:23 +0100

    Total cost

---
+++ BasicOperators.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+COST_PINEAPPLE=50
+
+COST_BANANA=4
+
+COST_WATERMELON=23
+
+COST_BASKET=1
+
+TOTAL=$(($COST_PINEAPPLE + $COST_BANANA * 2 + $COST_WATERMELON * 3 + $COST_BASKET))
+
+# Pour utiliser les opérateurs basic, les mettre entre parenthèses de parenthèses.
+# $ devant la parenthèse et devant chaque variable a énuméré
+
+echo "Total Cost is $TOTAL" 128
